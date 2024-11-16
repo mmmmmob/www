b95153036e2a8cc7420a0e40b64350066858f392
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-11-16 16:19:56 +0700

    feat: finish core component for the entire page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import Footer from "./components/Footer.tsx";
-import Header from "./components/Header.tsx";
-import Info from "./components/Info.tsx";
-import Title from "./components/Title.tsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Info from "./components/Info";
+//import { NowPlaying } from "./components/NowPlaying";
+import Project from "./components/Projects";
+import Title from "./components/Title";
+import WorkExp from "./components/WorkExp";
 
-const App = () => {
+function App() {
   return (
-    <div className="flex min-h-screen flex-col bg-slate-200 font-fkDisplay text-slate-800 dark:bg-slate-900 dark:text-slate-300">
-      <Header />
-      <Title />
-      <Info />
-      <Footer />
-    </div>
+    <>
+      <div className="flex min-h-screen flex-col bg-slate-200 text-slate-800 dark:bg-slate-900 dark:text-slate-300">
+        <header className="sticky top-0 w-full">
+          <Header />
+        </header>
+        <main className="flex flex-1 flex-col items-center justify-center">
+          <div className="my-6 flex w-full flex-col items-center justify-center sm:max-w-5xl">
+            <Title />
+            <hr className="m-5 h-px self-center border-0 bg-gray-400 max-sm:w-11/12 md:w-8/12 dark:bg-gray-600" />
+            <Info />
+            <WorkExp />
+            <Project />
+          </div>
+        </main>
+        <footer className="sticky bottom-0 w-full">
+          {/*<NowPlaying />*/}
+          <Footer />
+        </footer>
+      </div>
+      <SpeedInsights />
+      <Analytics />
+    </>
   );
-};
+}
 
 export default App;
