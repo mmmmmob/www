601f340d1d0bde7aa53b331761fc1e7bff20e2d7
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2025-05-10 19:19:11 +0700

    style: move icon to the center

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,24 @@ import ModeToggle from "./snippets/ModeToggle.tsx";
 
 const Header = () => {
   return (
-    <div className="flex h-12 w-screen justify-between bg-white/20 shadow backdrop-blur-lg md:h-14 dark:bg-gray-700/50">
-      <a
-        href="#"
-        className="ml-1 size-10 -translate-y-1 self-center rounded-md p-1 md:ml-3"
-      >
-        <img src="/logo.png" alt="avatar" />
-      </a>
-      <div className="mr-3 flex self-center md:mr-5">
-        <div className="self-center">
-          <CurrentTime />
+    <div className="relative flex h-12 w-screen items-center bg-white/20 shadow backdrop-blur-lg md:h-14 dark:bg-gray-700/50">
+      <div className="flex w-full justify-between px-5">
+        <div className="flex-none self-center font-fkDisplay text-sm font-bold">
+          <a href="#">Blogs</a>
         </div>
-        <div className="ml-2 self-center">
-          <ModeToggle />
+        <div className="flex flex-none self-center">
+          <div className="self-center max-md:hidden">
+            <CurrentTime />
+          </div>
+          <div className="ml-2 self-center">
+            <ModeToggle />
+          </div>
         </div>
+      </div>
+      <div className="absolute left-1/2 -translate-x-1/2">
+        <a href="#" className="block size-10 -translate-y-1 rounded-md p-1">
+          <img src="/logo.png" alt="avatar" />
+        </a>
       </div>
     </div>
   );
