c3400ad6bd937fad61e19326c51293a013d86fbb
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2025-05-10 20:35:16 +0700

    fix: change <a> to <Link>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import CurrentTime from "./snippets/CurrentTime.tsx";
 import ModeToggle from "./snippets/ModeToggle.tsx";
 
@@ -6,7 +7,7 @@ const Header = () => {
     <div className="relative flex h-12 w-screen items-center bg-white/20 shadow backdrop-blur-lg md:h-14 dark:bg-gray-700/50">
       <div className="flex w-full justify-between px-5">
         <div className="flex-none self-center font-fkDisplay text-sm font-bold">
-          <a href="/blogs">Blogs</a>
+          <Link to="/blogs">Blogs</Link>
         </div>
         <div className="flex flex-none self-center">
           <div className="self-center max-md:hidden">
@@ -18,9 +19,9 @@ const Header = () => {
         </div>
       </div>
       <div className="absolute left-1/2 -translate-x-1/2">
-        <a href="/" className="block size-10 -translate-y-1 rounded-md p-1">
+        <Link to="/" className="block size-10 -translate-y-1 rounded-md p-1">
           <img src="/logo.png" alt="avatar" />
-        </a>
+        </Link>
       </div>
     </div>
   );
