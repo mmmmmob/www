738c3f5e30a959ce0aa735ffe5bae735f4720180
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2025-05-24 20:33:27 +0700

    fix: dynamic rendering as asynchronous

--- next/src/app/blogs/[slug]/page.tsx
+++ next/src/app/blogs/[slug]/page.tsx
@@ -8,11 +8,10 @@ export async function generateStaticParams() {
   return slugs.map((slug) => ({ slug }));
 }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
+export async function generateMetadata(props: {
+  params: Promise<{ slug: string }>;
 }): Promise<Metadata> {
+  const params = await props.params;
   const post = await getPostBySlug(params.slug);
   if (!post) return {};
 
@@ -40,11 +39,10 @@ export async function generateMetadata({
   };
 }
 
-export default async function BlogPost({
-  params,
-}: {
-  params: { slug: string };
+export default async function BlogPost(props: {
+  params: Promise<{ slug: string }>;
 }) {
+  const params = await props.params;
   const post = await getPostBySlug(params.slug);
   if (!post) notFound();
   return (
